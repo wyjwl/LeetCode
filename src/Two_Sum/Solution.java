b55b14974d526590c619850f6e4c0760b36cd6fa
yijwang <yijwang@thoughtworks.com>
2018-07-17 20:25:29 +0800

    add solution

--- src/Two_Sum/Solution.java
+++ src/Two_Sum/Solution.java
@@ -12,9 +12,21 @@ package Two_Sum;
 //        因为 nums[0] + nums[1] = 2 + 7 = 9
 //        所以返回 [0, 1]
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-
+        Map<Integer, Integer> map = new HashMap<>();
+        int temp;
+        for(int i = 0; i < nums.length; i++){
+            temp = target - nums[i];
+            if(map.containsKey(temp)){
+                return new int[]{map.get(temp), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
     }
 
 }
